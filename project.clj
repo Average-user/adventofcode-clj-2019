0eaf6bffdc916311cf01bf17801dacfa49f6b418
Average-user <lucaspolymeris@protonmail.com>
2019-12-01 02:54:14 -0300

    fixed untracked files

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject adventofcode-clj-2019 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "solutions to aoc2019 www.adventofcode.com"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot adventofcode-clj-2019.core
   :target-path "target/%s"
