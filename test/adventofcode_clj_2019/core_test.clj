8ea88e0501de694d675f2834610226f773230e0d
Average-user <lucaspolymeris@protonmail.com>
2019-12-01 02:50:14 -0300

    tests

--- test/adventofcode_clj_2019/core_test.clj
+++ test/adventofcode_clj_2019/core_test.clj
@@ -1,7 +1,6 @@
 (ns adventofcode-clj-2019.core-test
   (:require [clojure.test :refer :all]
-            [adventofcode-clj-2019.core :refer :all]))
+            [adventofcode-clj-2019.day01 :as day01]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest day01-part-1 (is (= (day01/part-1) 3382136)))
+(deftest day01-part-2 (is (= (day01/part-2) 5070314)))
