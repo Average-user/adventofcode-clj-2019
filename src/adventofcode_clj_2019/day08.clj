43d2ffd0407932fb01ea125da8876424aa4fd5e0
Average-user <lucaspolymeris@protonmail.com>
2019-12-08 02:40:31 -0300

    day08 udate

--- src/adventofcode_clj_2019/day08.clj
+++ src/adventofcode_clj_2019/day08.clj
@@ -6,14 +6,14 @@
   (partition 150 (cs/trim (u/input 8))))
 
 (defn part-1 []
-  (let [frs (->> layers                 
-                 (sort-by #(get (frequencies %) \0))
-                 (first)
-                 (frequencies))]
+  (let [frs (->> layers
+                 (map frequencies)
+                 (sort-by #(get % \0))
+                 (first))]
     (* (frs \2) (frs \1))))
 
 (defn part-2 []
-  (->> (apply (partial map vector) layers)
+  (->> (apply (partial map list) layers)
        (map (comp {\0 \space \1 \#} #(first (drop-while (partial = \2) %))))
        (partition 25)
        (mapv (partial apply str))))
